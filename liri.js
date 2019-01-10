b9a576d8d41e6123cc79ddcfe2b00397dbad0652
Mtnman <billman.designs@gmail.com>
2019-01-10 15:48:05 -0500

    finished / possible refactor in future & log data to txt document

--- liri.js
+++ liri.js
@@ -11,20 +11,28 @@ var fs = require("fs");
 var command = process.argv[2];
 var input = process.argv.slice(3).join(" ");
 
+var logArry;
+
 // --------- Functions --------- //
 function fruit(){
+
+    //Researching Polymorphism for a possible refactor
     switch (command) {
         case "concert-this":
             concert();
+            //logData();
             break;
         case "spotify-this-song":
             spot();
+            //logData();
             break;
         case "movie-this":
             movie();
+            //logData();
             break;
         case "do-what-it-says":
             whatSays();
+            //logData();
             break;
         default:    
         console.log("\nGive me a command, fruit!\n");
@@ -58,11 +66,11 @@ function movie(){
         if (input === ""){
             console.log("\n\nIf you haven't watched 'Mr. Nobody,' then you probably shouldn't\nhttp://www.imdb.com/title/tt0485947/\nIt might be on Netflix!\n\n");
         } else {
-            console.log(data);
+            //console.log(data);
             console.log("\n\nMovie title: " + data.Title 
             + "\nYear: " + data.Year
             + "\nIMDB rating: " + data.imdbRating
-            + "\nRotten Tomatoes rating: " + data.Rating            //need to fix undefined
+            + "\nRotten Tomatoes rating: " + data.Ratings[1].Value
             + "\nProduced in: " + data.Country
             + "\nLanguage: " + data.Language
             + "\nPlot: " + data.Plot
@@ -83,10 +91,10 @@ function spot(){
         spotify.search({type: "track", query: input})
         .then(function(response){
             data = response.tracks.items[0];
-            console.log(data.album.artists);
-            console.log("\n\nArtist: " + data.album.artists.name    //need to fix undefined
+            //console.log(data);
+            console.log("\n\nArtist: " + data.album.artists[0].name
             + "\nSong: " + data.name
-            + "\nLink to song: " + data                             //need to fix undefined
+            + "\nLink to song: " + data.album.external_urls.spotify
             + "\nAlbum: " + data.album.name + "\n\n");
         })
     }
@@ -97,6 +105,16 @@ function whatSays(){
         var arrData = data.split(", ");
         command = arrData[0];
         input = arrData[1];
+        console.log("\n\n" + command + " " + input);
+        
         fruit();
     });
+}
+
+
+function logData(){
+    fs.appendFile("log.txt", logArry, function(err){
+        if (err) throw err;
+        console.log("Saved!");
+    })
 }
